280d87b308fb6ca95046c98cf2ad11d3ccf97e34
Maya Hampton <mhampton2003@gmail.com>
2022-04-01 21:35:24 -0700

    final project

---
+++ src/app/MyThread1.java
@@ -0,0 +1,14 @@
+package app;
+
+import java.lang.Thread;
+
+public class MyThread1 extends Thread {
+	
+	/**
+	 * runs the thread using the Thread class
+	 */
+	public void run() {
+		System.out.println("MyThread1 is running");
+	}
+
+}
---
+++ src/app/MyThread2.java
@@ -0,0 +1,12 @@
+package app;
+
+public class MyThread2 implements Runnable {
+	
+	/**
+	 * runs the thread using the Runnable Interface
+	 */
+	public void run() {
+		System.out.println("MyThread2 is running");
+	}
+
+}
